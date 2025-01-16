225462e85c1961037d0399d0d1b42a8f5d26b6cf
Nursultan <atamuratovnursultan12@gmail.com>
2025-01-17 03:10:07 +0500

    9-example

---
+++ Lab1/exString.py
@@ -0,0 +1,23 @@
+str1 = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = "Hello, World!"
+print(a.lower())
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+txt1 = "We are the so-called \"Vikings\" from the north."
+
