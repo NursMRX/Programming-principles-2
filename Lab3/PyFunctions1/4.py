39606e0b4c27ed3e9c086a4c93437b64f2277a5c
Nursultan <atamuratovnursultan12@gmail.com>
2025-01-29 13:25:13 +0500

    4 task:

---
+++ Lab3/PyFunctions1/4.py
@@ -0,0 +1,22 @@
+from random import randint
+from math import sqrt
+
+def filter_prime(numbers):
+    PrimeNumbers = []
+    for num in numbers:
+        if num < 2:  
+            continue
+        check = True
+        for i in range(2, round(sqrt(num)) + 1):
+            if num % i == 0: 
+                check = False
+                break
+        if check:
+            PrimeNumbers.append(num)
+    return PrimeNumbers
+
+
+numbers = [randint(1, 25) for n in range(25)]
+
+print(f"Generated numbers: {numbers}")
+print(f"Prime numbers: {filter_prime(numbers)}")
