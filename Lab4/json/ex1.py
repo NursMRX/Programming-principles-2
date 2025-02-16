0939c8b033b0cce37512ccd5d0db76bbe5913a9a
Nursultan <atamuratovnursultan12@gmail.com>
2025-02-17 00:22:48 +0500

    json

---
+++ Lab4/json/ex1.py
@@ -0,0 +1,27 @@
+import json
+
+with open("file.json", "r") as json_file:
+    json_data = json.load(json_file)
+    
+interfaces = json_data["imdata"]
+
+print("Interface Status")
+print("================================================================================")
+print("DN                                                 Description           Speed    MTU")  
+print("-------------------------------------------------- --------------------  ------  ------")
+
+def data(n):
+    print(n["dn"], end = "       ")
+    print(n["descr"], end= "                        ")
+    print(n["speed"], end = "  ")
+    print(n["mtu"])
+for i in interfaces:
+    data1 = i["l1PhysIf"]
+    data2 = data1["attributes"]
+    if data2["dn"] == "topology/pod-1/node-201/sys/phys-[eth1/33]":
+        data(data2)
+    if data2["dn"] == "topology/pod-1/node-201/sys/phys-[eth1/34]":
+        data(data2)
+    if data2["dn"] == "topology/pod-1/node-201/sys/phys-[eth1/35]":
+        data(data2)
+
