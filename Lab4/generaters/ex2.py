3427c9bd58a963aba43859177590cfa5e50cb778
Nursultan <atamuratovnursultan12@gmail.com>
2025-02-16 23:34:31 +0500

    even numbers find

---
+++ Lab4/generaters/ex2.py
@@ -0,0 +1,18 @@
+def even_numbers(num):
+    for num in range(1, num + 1):
+        if num % 2 == 0:
+            yield num
+        else:
+            continue
+
+number = int(input("Enter the endpoint number: "))
+it = even_numbers(number)
+
+try:
+    next_even = next(it)
+    while True:
+        print(next_even, end="" )
+        next_even = next(it)
+        print(", " if next_even is not None else "", end="")
+except StopIteration:
+    pass
