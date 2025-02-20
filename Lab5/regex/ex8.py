6ced3e45d19fa8c47211e2b7f1319ec012165dbc
Nursultan <atamuratovnursultan12@gmail.com>
2025-02-21 01:06:05 +0500

    task 8

--- Lab5/regex/ex8.py
+++ Lab5/regex/ex8.py
@@ -1,6 +1,7 @@
 import re
 with open("row.txt", "r", encoding="utf-8") as row:
     file = row.read()
-a = re.split(r"(?=[A-ZА-Я])", file)
-for i in a:
+
+regex = re.split(r"(?=[A-ZА-Я])", file)
+for i in regex:
     print (i, end=" ")
