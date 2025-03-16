3cd504ad190e386e61104173f1627ab97ee04755
Nursultan <atamuratovnursultan12@gmail.com>
2025-03-16 19:31:40 +0500

    practice

--- Lab5/reh/practice.py
+++ Lab5/reh/practice.py
@@ -1,5 +1,23 @@
-import re 
-text = "sdfghf"
-a = re.findall(r'[a-z]', text)
-for i in a:
-    print("_".join(a), end=" ")
+# import re 
+text = "sdfghfGemfkfmKefeafdfadfedfb"
+
+# pattern = r"a.*b"
+
+# a = re.split(r"(?=[A-ZА-Я])", text)
+
+# for i in a:
+#     print(i, end=" ")
+import re
+def spaces(text):
+    res = ""
+    pattern = re.compile(r"[a-zа-я\d]+|[A-ZА-Я][a-zа-я]*")
+    words = pattern.findall(text)
+    for i, word in enumerate(words):
+        if i != 0:
+            res += " " + word
+        else:
+            res += word
+    return res
+
+a = spaces(text)
+print(a)
